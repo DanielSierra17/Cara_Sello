894e4e12a68e0dec02a8a11365371bd2e78c9021
DanielSierra17 <dfsierra898@misena.edu.co>
2022-10-31 16:09:06 -0500

    Primer avance css interfaz

--- js/app.js
+++ js/app.js
@@ -1,30 +1,30 @@
-let boton = document.getElementById("bt")
+// let boton = document.getElementById("bt")
 
-function juego() {
+// function juego() {
 
-    let dinero = document.getElementById("pl").value
-    dinero = parseInt(dinero)
-    let repetir = document.getElementById("vj").value
-    repetir = parseInt(repetir)
-    let moneda = document.getElementById("op").value
-    let aleatorio = 1 + (Math.random() * 2)
+//     let dinero = document.getElementById("pl").value
+//     dinero = parseInt(dinero)
+//     let repetir = document.getElementById("vj").value
+//     repetir = parseInt(repetir)
+//     let moneda = document.getElementById("op").value
+//     let aleatorio = 1 + (Math.random() * 2)
 
-    for (i = 1; i <= repetir; i++) {
+//     for (i = 1; i <= repetir; i++) {
 
-        if (moneda == aleatorio) {
-            let total = dinero + dinero
-            Swal.fire(`Felicitaciones has ganado, el dinero que llevas en este momento es ${total}`)
-        }
+//         if (moneda == aleatorio) {
+//             let total = dinero + dinero
+//             Swal.fire(`Felicitaciones has ganado, el dinero que llevas en este momento es ${total}`)
+//         }
 
-        else {
-            total = dinero - dinero
-            Swal.fire(`Lastima has perdido, el dinero que llevas en este momento es ${total}`)
-        }
+//         else {
+//             total = dinero - dinero
+//             Swal.fire(`Lastima has perdido, el dinero que llevas en este momento es ${total}`)
+//         }
 
-        Swal.fire(`El dinero que ganaste jugando fue ${total} y jugaste ${i} veces`)
+//         Swal.fire(`El dinero que ganaste jugando fue ${total} y jugaste ${i} veces`)
 
-    }
+//     }
 
-}
+// }
 
-boton.addEventListener('click', juego)
+// boton.addEventListener('click', juego)
